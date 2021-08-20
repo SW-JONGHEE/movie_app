698afee22cbb7fcc4339d7ae0b9044d0734d7c63
jonghee <jonghee@nextculture.kr>
2021-08-20 16:31:20 +0900

    prop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, rating}) {
+    return (
+        <div>
+            <div>I Love {name}</div>
+            <h4>{rating}/5.0</h4>
+        </div>
+    );
+}
+
+function renderFood(dish) {
+    return <Food key={dish.id} name={dish.name} rating={dish.rating}/>
+}
+
+const foodILike = [
+    {
+        id:1,
+        name:'kimbap',
+        image: '',
+        rating: 5
+    },
+    {
+        id:2,
+        name:'cupnoodle',
+        image: '',
+        rating: 4.9
+    },
+    {
+        id:3,
+        name:'coffe',
+        image: '',
+        rating: 5.5
+    },
+    {
+        id:4,
+        name:'ia',
+        image: '',
+        rating: 3
+    },
+    {
+        id:5,
+        name:'il',
+        image: '',
+        rating: 4
+    },
+];
+
+Food.prototypes = {
+    name: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Hello!!</h1>
+        {foodILike.map(renderFood)}
     </div>
+
   );
 }
 
